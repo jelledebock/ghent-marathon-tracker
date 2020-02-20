6e6ddf10f55644ff02a53be435748c5b21a9db97
Jelle De Bock <jelle.debock@ugent.be>
2020-02-20 11:18:26 +0100

    Race simulator now working, and webserver reads locations via GET call and MQTT

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 // in sublime
 var express = require('express');
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://broker.hivemq.com')
 
 var port = process.env.PORT || 3000;
 
 var app = express();
+var mqtt_listener;
+var curr_location = {};
+
+app.get('/location', function (req, res) {
+  res.json(curr_location);
+});
 
 app.get('/', function (req, res) {
-  res.send(JSON.stringify({ Hello: 'World'}));
+  res.send('Hello world!');
+});
+app.listen(port, function () {
+  console.log("App started listening on "+port+"!");
 });
 
-app.listen(port, function () {
- console.log('Example app listening on port '+port+'!');
+client.on('connect', ()=>{
+  console.log("Connected to MQTT location stream.");
+  client.subscribe('owntracks/ghentmarathon/+');
 });
+
+client.on('message', (topic, message) => {
+  var athlete = topic.split('/').pop();
+  var location_obj = JSON.parse(message.toString());
+  console.log("Got location update of "+athlete);
+  if(!(curr_location[athlete])){
+    curr_location[athlete]={"latitude": -1, "longitude": -1, "timestamp": -1};
+  }
+  curr_location[athlete].latitude=location_obj.lat
+  curr_location[athlete].longitude = location_obj.lon
+  curr_location[athlete].timestamp = location_obj.tst
+  
+  console.log(message.toString());
+})
--- marathon-simulator/simulate_race.py
+++ marathon-simulator/simulate_race.py
@@ -5,9 +5,11 @@ from datetime import datetime, timedelta
 import pandas as pd
 import time
 import json
+import random
 
 sample_json = {"bs":1,"tst":1581948338,"acc":65,"_type":"location","alt":3,"lon":3.7098169473835059,"vac":11,"lat":51.01335531674944,"batt":86,"conn":"w","tid":"ab"}
 topic_name = 'owntracks/ghentmarathon/{}'
+ABDI_GOAL_DURATION = 3960
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -33,8 +35,39 @@ def process_gpx_file(gpx_path):
                 points.append([point.latitude, point.longitude, point.time, (point.time-start).total_seconds(), cum_dist])
     return points
 
-def broacast_mqtt_location(athlete, client, df, athlete_speed, total_elapsed_for_athlete):
-    client.publish(topic_name.format(athlete), json.dumps(sample_json))
+def find_row(df, cum_dist):
+    df_copy = df.copy()
+    df_copy['prev_dist']=df_copy['distance'].shift(1)
+    if cum_dist>0:
+        row = df.loc[(df_copy.prev_dist<cum_dist) & (df_copy.distance>=cum_dist)].iloc[0]
+    elif cum_dist>df.iloc[-1]['distance']:
+        row = df.iloc[-1]
+    else:
+        row = df.iloc[0]
+    return row
+
+def randrange_float(start, stop, step):
+    return random.randint(0, int((stop - start) / step)) * step + start
+
+def broacast_mqtt_location(athlete, client, df, cum_dist, run_time, timestamp_now):
+    row_obj = find_row(df, cum_dist)
+    output = sample_json.copy()
+    output['lat']=row_obj['lat']
+    output['lon']=row_obj['lon']
+    output['tst']=int(time.mktime(timestamp_now.timetuple()))
+    print(output)
+    client.publish(topic_name.format(athlete), json.dumps(output))
+
+def get_finish_time_prediction(distance_completed, total_distance, elapsed_time_seconds, timestamp_now):
+    if distance_completed>0 and elapsed_time_seconds>0:
+        speed = distance_completed/elapsed_time_seconds
+        distance_remaining = total_distance-distance_completed
+
+        time_remaining = distance_remaining/speed
+        
+        return time_remaining, timestamp_now+timedelta(seconds=time_remaining)
+    else:
+        return ABDI_GOAL_DURATION, timestamp_now+timedelta(seconds=ABDI_GOAL_DURATION)
 
 if __name__ == "__main__": 
     client = mqtt.Client()
@@ -49,6 +82,8 @@ if __name__ == "__main__":
     points_df['timestamp']=pd.to_datetime(points_df['timestamp'])
     points_df = points_df.set_index('timestamp')
     points_df = points_df.resample('1s').interpolate(method='linear')
+    exact_distance = points_df.iloc[-1]['distance']
+    print("Total distance of the course ", exact_distance)
     start_date = datetime.now()
     # Simulate abdi running at 18.5 kph and starting 10min after first runner (15kph)
     speed_abdi = float(sys.argv[2])/3.6
@@ -56,25 +91,48 @@ if __name__ == "__main__":
     time_gap_start_min = float(sys.argv[4])
     time_gap_start_sec = 60*time_gap_start_min
 
-    abdi_total_time = time_gap_start_sec + points_df.iloc[-1]['distance']/speed_abdi
-    first_runner_total_time = points_df.iloc[-1]['distance']/speed_first_runner
-
-    print("Abdi will arrive from now in {} seconds.".format(abdi_total_time))
-    print("First will arrive from now in {} seconds.".format(first_runner_total_time))
+    abdi_total_time = time_gap_start_sec+exact_distance/speed_abdi
+    first_runner_total_time = exact_distance/speed_first_runner
 
     curr_elapsed_time_s = 0
-    abdi_elapsed_running_time = 0
-    first_runner_elapsed_running_time = 0
-    while curr_elapsed_time_s<max(abdi_total_time, first_runner_total_time):
-        print('Time', start_date+timedelta(seconds=curr_elapsed_time_s))
-        print("Run time of Abdi: ", abdi_elapsed_running_time)
-        print("Run time of first runner: ", first_runner_elapsed_running_time)
-        first_runner_elapsed_running_time+=1
+    abdi_previous_s_distance = 0
+
+    abdi_cum_distance = 0
+    abdi_run_time = 0
+
+    first_runner_cum_dist = 0
+    first_runner_run_time = 0
+
+    while min(first_runner_cum_dist, abdi_cum_distance)<exact_distance:
+        timestamp_now = start_date+timedelta(seconds=curr_elapsed_time_s)
+        print('Time', timestamp_now)
+        print('Distance traveled previous second: ')
+        print("Run time of Abdi: ", abdi_run_time)
+        print("Run distance of Abdi: ", abdi_cum_distance)
+        print("Run time of first runner: ", first_runner_run_time)
+        print("Run distance of first runner: ", first_runner_cum_dist)
         # Broadcast location via MQTT 
-        broacast_mqtt_location('abdi', client, points_df, speed_abdi, abdi_elapsed_running_time)
-        broacast_mqtt_location('first_runner', client, points_df, speed_first_runner, first_runner_elapsed_running_time)
+        broacast_mqtt_location('abdi', client, points_df, abdi_cum_distance, abdi_run_time, timestamp_now)
+        broacast_mqtt_location('first_runner', client, points_df, first_runner_cum_dist, first_runner_run_time, timestamp_now)
+        
         curr_elapsed_time_s+=1
-        if curr_elapsed_time_s>time_gap_start_sec:
-            abdi_elapsed_running_time+=1
+        if curr_elapsed_time_s>time_gap_start_sec and abdi_cum_distance<=exact_distance:
+            abdi_run_time+=1
+            distance_traveled_s = randrange_float((speed_abdi-0.55), (speed_abdi+0.55), 0.1)
+            abdi_cum_distance+=distance_traveled_s
+        
+        if first_runner_cum_dist<=exact_distance:
+            first_runner_run_time+=1
+            distance_traveled_s = randrange_float((speed_first_runner-0.55), (speed_first_runner+0.55), 0.1)
+            first_runner_cum_dist+=distance_traveled_s
+
+        if curr_elapsed_time_s%30==0:
+            time_remaining_abdi, arrival_datetime_abdi = get_finish_time_prediction(abdi_cum_distance, exact_distance, abdi_run_time, timestamp_now)
+            time_remaining_first_runner, arrival_datetime_first_runner = get_finish_time_prediction(first_runner_cum_dist, exact_distance, first_runner_run_time, timestamp_now)
+
+            print("Predictions on current pace (last 30 seconds):")        
+            print("Abdi arrival prediction: {}({})".format(time_remaining_abdi, arrival_datetime_abdi))
+            print("First runner arrival prediction: {} ({})".format(time_remaining_first_runner, arrival_datetime_first_runner))
+
         time.sleep(1)
 
