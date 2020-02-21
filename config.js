ae269f7f4a0eb6d103241b3f97790523b2fb3e52
Jelle De Bock <jelle.debock@ugent.be>
2020-02-21 08:01:15 +0100

    Code cleanup

--- config.js
+++ config.js
@@ -4,7 +4,17 @@ config.twitter = {};
 config.redis = {};
 config.web = {};
 
-config.gpx_path = 'parcours.gpx'
-config.tracker_ids = ['abdi', 'first_runner']
+config.gpx_path = 'parcours.gpx';
+config.tracker_ids = ['abdi', 'first_runner'];
+config.firebaseConfig = {
+    apiKey: "AIzaSyDoC5ikV2AIDV2ut64mue22UqXudrryj9A",
+    authDomain: "gps-tracker-443da.firebaseapp.com",
+    databaseURL: "https://gps-tracker-443da.firebaseio.com",
+    projectId: "gps-tracker-443da",
+    storageBucket: "gps-tracker-443da.appspot.com",
+    messagingSenderId: "157616821882",
+    appId: "1:157616821882:web:ce95258fe35b9360a68dc3",
+    measurementId: "G-9RLE2BLSKT"
+};
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 // in sublime
+var gpx = require('./lib/gpx-controller');
+var mqtt = require('./lib/mqtt-controller');
 var express = require('express');
 var config = require('./config');
-const mqtt = require('mqtt');
-const gpxParse = require('parse-gpx');
-const haversine = require('haversine');
+var passport = require('passport');
 
-var client;
 var port = process.env.PORT || 3000;
-
 var app = express();
-var curr_location = {};
-var parcours = {};
+
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/config');
+  });
+
+app.get('/config',  passport.authenticate('local'), function(req, res){
+  res.json(config);
+})
 
 app.get('/location', function (req, res) {
-  res.json(curr_location);
+  res.json(mqtt.current_location);
 });
 
 app.get('/parcours', function (req, res) {
-  res.json(parcours);
+  res.json(gpx.parcours);
 });
 
 app.get('/', function (req, res) {
@@ -26,47 +34,7 @@ app.get('/', function (req, res) {
 
 app.listen(port, function () {
   console.log("App started listening on "+port+"!");
-  console.log(config.gpx_path);
-  gpxParse(config.gpx_path).then(track =>{
-    var calc_dist = 0;
-    parcours['points']=[];
-    for(var i=0; i<track.length; i++){
-      if(i>0){
-        lat1 = track[i-1].latitude;
-        lon1 = track[i-1].longitude;
-        lat2 = track[i].latitude;
-        lon2 = track[i].longitude;
-        ele = track[i].elevation;
-        calc_dist+=haversine({'latitude':lat1, 'longitude':lon1}, {'latitude':lat2, 'longitude': lon2}, {unit: 'meter'});
-        parcours['points'].push([lat2, lon2, ele]);
-        //console.log(parcours);
-        //console.log('Cumm distance '+calc_dist);
-      }
-    }
-    parcours['total_distance'] = calc_dist;
-  });
-
-  client = mqtt.connect('mqtt://broker.hivemq.com');
-  client.on('connect', ()=>{
-    console.log("Connected to MQTT location stream.");
-    client.subscribe('owntracks/ghentmarathon/+');
-  });
-  
-  client.on('message', (topic, message) => {
-    var athlete = topic.split('/').pop();
-    var location_obj = JSON.parse(message.toString());
-    console.log("Got location update of "+athlete);
-    if(!(curr_location[athlete]) && config.tracker_ids.includes(athlete)){
-      curr_location[athlete]={"latitude": -1, "longitude": -1, "timestamp": -1};
-    }
-    if(config.tracker_ids.includes(athlete)){
-      curr_location[athlete].latitude=location_obj.lat
-      curr_location[athlete].longitude = location_obj.lon
-      curr_location[athlete].timestamp = location_obj.tst
-    }
-
-    
-    console.log(message.toString());
-  })
+  mqtt.initialize();
+  gpx.intialize();
 });
 
